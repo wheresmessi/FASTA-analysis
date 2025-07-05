932d68ccd3de7fa982afffea2d83ad4ad827467b
nanda kumaran <129156245+wheresmessi@users.noreply.github.com>
2025-07-05 14:19:52 +0530

    Update app.py
    localhost to 0.0.0.0

--- backend/app.py
+++ backend/app.py
@@ -81,4 +81,6 @@ def serve_react(path):
 
 # === Run Flask ===
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 locally
+    app.run(host="0.0.0.0", port=port)
+
